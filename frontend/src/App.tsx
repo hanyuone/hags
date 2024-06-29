d2eee41a0d0abf4ebeae81beb874dccf9089748d
joanna209 <80564580+joanna209@users.noreply.github.com>
2024-06-29 12:54:42 +1000

    feat: setup frontend (#1)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button onClick={() => console.log('a sanity check')}>Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
