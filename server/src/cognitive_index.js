e199d1ea15fb754e07daf21036464f2735aaa187
Aolin Xu <aolinzamxu@gmail.com>
2024-06-29 14:54:30 +1000

    rgb to hsv func

---
+++ server/src/cognitive_index.js
@@ -0,0 +1,21 @@
+function rgbToHsv(r, g, b) {
+    r /= 255, g /= 255, b /= 255;
+    let max = Math.max(r, g, b), min = Math.min(r, g, b);
+    let h, s, v = max;
+
+    let d = max - min;
+    s = max == 0 ? 0 : d / max;
+
+    if (max == min) {
+        h = 0;
+    } else {
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, v];
+}
