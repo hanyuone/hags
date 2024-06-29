0711e82015da95880e7ca0f4d721a27e8ceacb36
Hanyuan Li <work@hanyuone.liv>
2024-06-29 15:02:30 +1000

    feat(server): add support for uploading images

--- server/src/main.js
+++ server/src/main.js
@@ -1,13 +1,24 @@
 import express from "express";
+import multer from "multer";
 
 const PORT = 3000;
+const UPLOAD_PATH = "uploads/";
+
+const upload = multer({ dest: UPLOAD_PATH });
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
+app.post("/cli", upload.single("screenshot"), (req, res) => {
+    const imagePath = req.file.path;
+
+    res.send({
+        // TODO: fill in brightness and temperature values
+        "brightness": 0,
+        "temperature": 0,
+        "filePath": imagePath,
+    });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server launching on port ${PORT}`)
+    console.log(`Server launching on port ${PORT}`);
 });
